86dead66c7c6ed8c48558a1a0762ebb17a9850f2
diego-ab <30775484+diego-ab@users.noreply.github.com>
2017-09-11 22:35:57 +0100

    dash rules

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -273,7 +273,7 @@ public:
         consensus.fStrictChainId = false;
         consensus.nLegacyBlocksBefore = -1;
 
-        consensus.nDashRulesStartHeight = 0;
+        consensus.nDashRulesStartHeight = 50500;
 
         pchMessageStart[0] = 0x0b;
         pchMessageStart[1] = 0x11;
