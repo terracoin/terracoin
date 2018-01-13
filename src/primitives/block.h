ffd75edd96ed964e15294d1dd4af9f46944cb276
Justin F. Hallett <thesin@southofheaven.org>
2018-01-13 07:16:31 -0700

    This is a fix for PR6

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -123,6 +123,7 @@ public:
         block.nTime          = nTime;
         block.nBits          = nBits;
         block.nNonce         = nNonce;
+        block.auxpow         = auxpow;
         return block;
     }
 
