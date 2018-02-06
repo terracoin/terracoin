de98c0704c3b06bdb78ad8d362ffbdc1e4ce8278
Justin F. Hallett <thesin@southofheaven.org>
2018-02-06 14:31:01 -0700

    line up the copyrights properly

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -85,7 +85,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
     pixPaint.setFont(QFont(font, 10*fontFactor));
     pixPaint.drawText(paddingLeft,paddingTop+titleCopyrightVSpace,copyrightTextBtc);
     pixPaint.drawText(paddingLeft,paddingTop+titleCopyrightVSpace+12,copyrightTextDash);
-    pixPaint.drawText(paddingLeft,paddingTop+titleCopyrightVSpace+12,copyrightTextTerracoin);
+    pixPaint.drawText(paddingLeft,paddingTop+titleCopyrightVSpace+24,copyrightTextTerracoin);
 
     // draw additional text if special network
     if(!titleAddText.isEmpty()) {
