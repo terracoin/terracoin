4a7b5049c781a55fde8d0bfe1f534f3580337ccb
Justin F. Hallett <thesin@southofheaven.org>
2018-02-06 18:04:46 -0700

    Move text up slightly, so it doesn't end so low

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -36,7 +36,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
 
     // set reference point, paddings
     int paddingLeft             = 14;
-    int paddingTop              = 470;
+    int paddingTop              = 442;
     int titleVersionVSpace      = 17;
     int titleCopyrightVSpace    = 32;
 
