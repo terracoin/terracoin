df03d6d64f2beb7e381f20450b669c9067d4f783
Justin F. Hallett <thesin@southofheaven.org>
2018-07-10 18:49:57 -0600

    Add a 0.12.2.3 ignore, with the current network status and 0.12.2.3's lack of ability to ban, it made a terribley loud echo

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1200,10 +1200,19 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             pfrom->fRelayTxes = fRelay; // set to true after we get the first filter* message
         }
 
+        // Terracoin as version based ignoring
         // START: Create a soft fork based on version
         int versparts[4];
         sscanf(pfrom->cleanSubVer.c_str(), "/Terracoin Core:%d.%d.%d.%d", &versparts[0], &versparts[1], &versparts[2], &versparts[3]);
         int intVersion = 1000000 * versparts[0] + 10000 * versparts[1] + 100 * versparts[2] + 1 * versparts[3];
+        // disconnect from 0.12.2.3 peers
+        if (intVersion == 120203)
+        {
+            LogPrintf("peer=%d using obsolete version %d; disconnecting\n", pfrom->id, intVersion);
+            connman.PushMessageWithVersion(pfrom, INIT_PROTO_VERSION, NetMsgType::REJECT, strCommand, REJECT_OBSOLETE, string("Version 0.12.2.3 banned"));
+            pfrom->fDisconnect = true;
+            return false;
+        }
         if (intVersion < MIN_PEER_VERSION)
         {
             // disconnect from peers older than this version
